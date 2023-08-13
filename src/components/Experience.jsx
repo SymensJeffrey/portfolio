4433922749cf3b7287d3cd19200cf7b573bd6176
symensjeffrey <jeffrey.symens23@gmail.com>
2023-08-12 19:30:40 -0500

    Add imports for experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
+import { motion } from "framer-motion"
+
+import 'react-vertical-timeline-component/style.min.css'
+
+import { styles } from "../style"
+import { experiences } from "../constants"
+import { SectionWrapper } from "../hoc"
+import { textVariant } from "../utils/motion"
 
 const Experience = () => {
   return (
